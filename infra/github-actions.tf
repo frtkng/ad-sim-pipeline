1f41d508d587a52511700663b954b17914a26842
KengoF <frtkng@gmail.com>
2025-06-24 23:21:57 +0900

    cict pipeline updated

--- infra/github-actions.tf
+++ infra/github-actions.tf
@@ -34,6 +34,7 @@ locals {
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser",
   ]
 }
 
