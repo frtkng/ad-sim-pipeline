0866a8c0ba702214658bc39f9b55a195427fa300
bean <35648235+frtkng@users.noreply.github.com>
2025-06-24 17:37:55 +0900

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,15 @@
-variable "aws_region" { default = "ap-northeast-1" }
+variable "aws_region" {
+  description = "デプロイ先の AWS リージョン"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "aws_account_id" {
+  description = "リソースARNに使うアカウントID"
+  type        = string
+  default     = "676206918971"
+}
+
 variable "env"        { default = "dev" }
 variable "ec2_key" {
   description = "Name of the EC2 key pair to use for the EKS nodes"
